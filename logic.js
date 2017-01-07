bfe8707540c1b08067c1c31b6d57122984b3fab5
josephgit45 <josephlee.45@gmail.com>
2017-01-07 11:37:01 -0800

    picture first first commit
    deleted chinese translator

---
+++ logic.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//     var response = '';
+//     $.ajax({ type: "GET",
+//              url: "http://labs.bible.org/api/?passage=random&type=json&callback=myfunction",
+//              async: false,
+//              dataType: 'jsonp',
+//              success : function(text)
+//              {
+//                  response = text;
+//              }
+//     });
+//     console.log(response);
+//  });
